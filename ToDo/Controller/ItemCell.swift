6a5684e5f754914c88b1f249958c36067d4ca35c
Atthachai <atthachai.mee@gmail.com>
2017-07-19 17:41:26 +0700

    Implementing ItemCell

--- ToDo/Controller/ItemCell.swift
+++ ToDo/Controller/ItemCell.swift
@@ -8,7 +8,30 @@
 import UIKit
 
 class ItemCell: UITableViewCell {
-  func configCell(with item: ToDoItem) {
-    
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  
+  lazy var dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MM/dd/yyyy"
+    return dateFormatter
+  }()
+  
+  func configCell(with item: ToDoItem, checked: Bool = false) {
+    if checked {
+      let attributeString = NSAttributedString(string: item.title, attributes: [NSAttributedStringKey.strikethroughStyle : NSUnderlineStyle.styleSingle.rawValue])
+      titleLabel.attributedText = attributeString
+      locationLabel.text = nil
+      dateLabel.text = nil
+    } else {
+      titleLabel.text = item.title
+      locationLabel.text = item.location?.name
+      if let timestamp = item.timestamp {
+        let date = Date(timeIntervalSince1970: timestamp)
+        dateLabel.text = dateFormatter.string(from: date)
+      }
+    }
   }
 }
